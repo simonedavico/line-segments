a250bb8be8ec8c1deaf02fbc0c6c66d24aa09e20
Matteo Codogno <matteo.codogno.90@gmail.com>
2018-06-03 19:37:02 +0200

    WIP on test suite

--- src/main/kotlin/io/github/linesegments/RestClient.kt
+++ src/main/kotlin/io/github/linesegments/RestClient.kt
@@ -1,16 +1,10 @@
 package io.github.linesegments
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.github.linesegments.bl.compareWithSnapshot
 import io.github.linesegments.bl.segments.Line
 import io.github.linesegments.dal.LinePatternRest
-import io.github.linesegments.dal.getPointFromJson
 import io.github.linesegments.dal.getPointsFromJsonString
 import io.github.linesegments.utility.Futures
-import io.vertx.core.json.JsonArray
-import io.vertx.core.json.JsonObject
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 import kotlin.system.exitProcess
@@ -18,10 +12,7 @@ import kotlin.system.exitProcess
 fun main(args: Array<String>) {
     println("Endpoint ${args[0]}")
 
-    val objectMapper = ObjectMapper(YAMLFactory())  // Enable YAML parsing
     val restClient = LinePatternRest(args[0])
-
-    objectMapper.registerModule(KotlinModule())     // Enable Kotlin support
 
     restClient
             .deleteSpace()
--- src/main/kotlin/io/github/linesegments/bl/SegmentValidator.kt
+++ src/main/kotlin/io/github/linesegments/bl/SegmentValidator.kt
@@ -1,16 +1,33 @@
 package io.github.linesegments.bl
 
+import io.github.linesegments.api.SimpleLineSegmentsService
 import io.github.linesegments.bl.segments.Line
+import io.github.linesegments.bl.segments.Point
 import io.github.linesegments.dal.getPointsFromYamlFile
 
 /**
  * @author Matteo Codogno on 03.06.18
  */
-fun compareWithSnapshot(segmentLength: Int, lines: Set<Line>) {
-    val snapshotLines = getPointsFromYamlFile<Set<Line>>("output_$segmentLength.yml")
+fun compareWithSnapshot(segmentLength: Int, lines: Set<Line>) = validate(segmentLength)(lines)(getPointsFromYamlFile("output_$segmentLength.yml"))
 
-    assert(snapshotLines.size == lines.size) { "$segmentLength - Divergent size" }
-    lines.map {
-        assert(snapshotLines.contains(it)) { "$segmentLength - $it does not exist in snapshot lines" }
+fun compareWithPrefixedInput(segmentLength: Int, lines: Set<Line>) = validate(segmentLength)(lines)(getPointsFromYamlFile("output_$segmentLength.yml"))
+
+//private fun getSegments(segmentLength: Int) : Set<Line> {
+//    val lineSegments = SimpleLineSegmentsService()
+//
+//    getPointsFromYamlFile<Set<Point>>("points_001.yml").map {
+//        lineSegments.addPoint(it)
+//    }
+//
+//    lineSegments.getLineSegments(segmentLength)
+//}
+
+private fun validate(segmentLength: Int) = {
+    lines: Set<Line> -> {
+        snapshotLines: Set<Line> ->
+            assert(snapshotLines.size == lines.size) { "$segmentLength - Divergent size" }
+            lines.map {
+                assert(snapshotLines.contains(it)) { "$segmentLength - $it does not exist in snapshot lines" }
+            }
     }
 }
